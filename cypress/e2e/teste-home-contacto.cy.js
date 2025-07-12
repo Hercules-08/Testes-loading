b38d26bcc26f6f9a09b1970b60a9cac6b8ab106d
Jhamison Rodrigues <jhamisonsilva@gmail.com>
2025-07-12 11:06:30 +0100

    Ajusta testes e corrige timeout do Cypress

--- cypress/e2e/teste-home-contacto.cy.js
+++ cypress/e2e/teste-home-contacto.cy.js
@@ -1,26 +1,40 @@
 describe('testes na home da loading', () => {
-	beforeEach(() => {
-		//cy.viewport('iphone-6'); //teste para telemovel
-		// Ignora o erro de jQuery undefined
-		Cypress.on('uncaught:exception', (err, runnable) => {
-			if (err.message.includes('jQuery is not defined')) {
-				return false; // Impede que o erro quebre o teste
-			}
-		});
+  beforeEach(() => {
+    Cypress.on('uncaught:exception', (err, runnable) => {
+      if (err.message.includes('jQuery is not defined')) {
+        return false;
+      }
+    });
 
-		// Visita o site com failOnStatusCode falso, para evitar quebra por erros de status
-		cy.visit('https://loadingsoftware.pt/', {
-			failOnStatusCode: false,
-			timeout: 90000 // timeout estendido se necessário
-		});
-	});
+    cy.visit('https://loadingsoftware.pt/', {
+      failOnStatusCode: false,
+      timeout: 90000,
+    });
+  });
 
-	it('testando todos os caminhos na pagina home e verificando', () => {
-		cy.contains('a', 'Contacto').click();
-		cy.get('#wpforms-4742-field_1').type('Harry Potter')
-		cy.get('#wpforms-4742-field_2').type('Harry')
-		cy.get('#wpforms-4742-field_3').type('Harry@email.com')
-		cy.get('#wpforms-4742-field_5').type('13 9314351')
-		cy.get('#wpforms-4742-field_10').type('Sou Harry só Harry')
-	});
+  it('testando todos os caminhos na pagina home e verificando', () => {
+    // Clica no link de Contacto e espera a navegação
+    cy.contains('a', 'Contacto').click();
+
+    // Espera o formulário estar visível
+    cy.get('#wpforms-4742-field_1', { timeout: 10000 })
+      .should('be.visible')
+      .type('Harry Potter');
+
+    cy.get('#wpforms-4742-field_2')
+      .should('be.visible')
+      .type('Harry');
+
+    cy.get('#wpforms-4742-field_3')
+      .should('be.visible')
+      .type('Harry@email.com');
+
+    cy.get('#wpforms-4742-field_5')
+      .should('be.visible')
+      .type('13 9314351');
+
+    cy.get('#wpforms-4742-field_10')
+      .should('be.visible')
+      .type('Sou Harry só Harry');
+  });
 });
