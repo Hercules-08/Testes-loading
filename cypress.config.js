b0a1a8d917f0ef375aeac75bc4562484a56f2c16
Jhamison Rodrigues <jhamisonsilva@gmail.com>
2025-07-12 10:36:12 +0100

    testes para rodar no actions

---
+++ cypress.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  e2e: {
+    baseUrl: 'https://loadingsoftware.pt/',
+    pageLoadTimeout: 120000,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+};
---
+++ cypress/e2e/testes-home.cy.js
@@ -0,0 +1,22 @@
+describe('testes na home da loading', () => {
+	beforeEach(() => {
+		//cy.viewport('iphone-6'); //teste para telemovel
+
+		// Ignora o erro de jQuery undefined
+		Cypress.on('uncaught:exception', (err, runnable) => {
+			if (err.message.includes('jQuery is not defined')) {
+				return false; // Impede que o erro quebre o teste
+			}
+		});
+
+		// Visita o site com failOnStatusCode falso, para evitar quebra por erros de status
+		cy.visit('https://loadingsoftware.pt/', {
+			failOnStatusCode: false,
+			timeout: 90000 // timeout estendido se necessário
+		});
+	});
+
+	it('testando todos os caminhos na pagina home e verificando', () => {
+		cy.contains('a', 'Serviços').click();
+	});
+});
